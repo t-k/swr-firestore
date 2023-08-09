4d8d19fa0c9664913c04be5d4a7bc542bd54c656
Tatsuo Kaniwa <tatsuo@kaniwa.biz>
2023-08-09 13:47:26 +0900

    replace coverage provider c8 to v8

--- tests/setup.ts
+++
@@ -1,4 +0,0 @@
-process.env.NODE_ENV = "test";
-process.env.FIREBASE_AUTH_EMULATOR_HOST = "localhost:9099";
-process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
-process.env.GCLOUD_PROJECT = "swr-firestore-project";
